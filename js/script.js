92a321e1153e4a7e26192dbcbdfe8b9d959170fb
Eduardo Fully Figueira <eduardofullyfigueira@gmail.com>
2023-05-10 16:10:17 -0300

    commit 1
    Sistema funcional de código de barra

---
+++ js/script.js
@@ -0,0 +1,25 @@
+Quagga.init({
+    inputStream : {
+      name : "Live",
+      type : "LiveStream",
+      target: document.querySelector('#tuamae')    // Or '#yourElement' (optional)
+    },
+    decoder : {
+      readers: [
+        'code_128_reader'
+      ]
+    }
+  }, function(err) {
+      if (err) {
+          console.log(err);
+          return
+      }
+      console.log("Initialization finished. Ready to start");
+      Quagga.start();
+      
+  });
+
+Quagga.onDetected(function(data){
+    console.log(data);
+    document.querySelector('#filho').innerHTML = data.codeResult.code;
+});
